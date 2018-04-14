2398da2effd8410d676da6450c87de32b3788a42
Roland Poot <roland.poot@pixelate.nl>
2018-04-14 14:38:18 +0200

    Fixed duplicate keys warning for live shows

--- pages/live.vue
+++ pages/live.vue
@@ -1,22 +1,23 @@
 <template>
-   <div class="columns">
-      <div class="column">
-  <section class="block">
-    <header class="block__header">
-      <h1 class="block__title">Live shows</h1>
-    </header>
-    <div class="block__content">
-       <div class="content">
-      <ul>
-        <li v-for="show in shows" v-bind:key="show">
-           <div> <strong>{{show.date}} - {{show.time}}</strong> - <a :href="show.url">{{show.title}}</a> </div>
-           <div>Venue: {{show.venue}} - {{show.city}} </div>
-        </li>
-      </ul>
-       </div>
-    </div>
-  </section>
-      </div></div>
+<div class="columns">
+  <div class="column">
+    <section class="block">
+      <header class="block__header">
+        <h1 class="block__title">Live shows</h1>
+      </header>
+      <div class="block__content">
+        <div class="content">
+            <ul>
+              <li v-for="(show, index) in shows" :key="index">
+                <div><strong>{{show.date}} - {{show.time}}</strong> - <a :href="show.url">{{show.title}}</a></div>
+                <div>Venue: {{show.venue}} - {{show.city}} </div>
+              </li>
+            </ul>
+        </div>
+      </div>
+    </section>
+  </div>
+</div>
 </template>
 <script>
   import shows from '~/static/shows.json'
